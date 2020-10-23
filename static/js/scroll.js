4be17c08c0d23706dfd638fa5c6b17c89c988391
PWashburn55 <pw325555@gmail.com>
2020-10-23 09:55:11 +0000

    added button to go to top of page - need to style

---
+++ static/js/scroll.js
@@ -0,0 +1,17 @@
+// code from W3 Schools for button to scroll to top
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
