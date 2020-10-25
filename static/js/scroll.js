3d84bc98a4019e8a4f0597905c380e9911d161a5
PWashburn55 <pw325555@gmail.com>
2020-10-25 19:57:42 +0000

    added links to footer & fixed navbar

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,6 +1,7 @@
 // code from W3 Schools for button to scroll to top
 mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,6 +12,7 @@ function scrollFunction() {
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
