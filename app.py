c742e1215ddc64fb66c8318c6308d5d02b3f531d
PWashburn55 <pw325555@gmail.com>
2020-11-03 11:47:41 +0000

    Remove env

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-if os.path.exists("env.py"):
-    import env
+#if os.path.exists("env.py"):
+#    import env
 
 app = Flask(__name__)
 
